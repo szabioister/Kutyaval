fb2b871dba5eb48a35966f662413d3e6960dde2f
Barta Szabolcs <szabioister@gmail.com>
2012-01-31 10:59:09 +0100

    login init

--- src/com/kutyaval/KutyavalActivity.java
+++ src/com/kutyaval/KutyavalActivity.java
@@ -1,5 +1,6 @@
 package com.kutyaval;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,22 +12,31 @@ public class KutyavalActivity extends FBConnectionActivity {
     private TextView txtUserName;
     private ProgressBar pbLogin;
     private Button btnLogin;
+    // ez a storage, ahol a pwd tarolodik
+    public static final String PREFS_NAME = "Kutyaval";
+    private String uid;
    
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.main);
 
-	    txtUserName = (TextView) findViewById(R.id.textFacebook);
-	    pbLogin = (ProgressBar) findViewById(R.id.progressLogin);
-	    btnLogin = (Button) findViewById(R.id.buttonLogin);
-	            btnLogin.setOnClickListener(new OnClickListener() {
-	                    @Override
-	                    public void onClick(View arg0) {
-	                            pbLogin.setVisibility(ProgressBar.VISIBLE);
-	                            setConnection();
-	                            getID(txtUserName, pbLogin);
-	                    }
-	            });
+	    // Restore preferences
+	    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+	    uid = settings.getString("uid", "");
+	    if(uid=="") {
+	    	setContentView(R.layout.main);
+	    	
+	    	txtUserName = (TextView) findViewById(R.id.textFacebook);
+		    pbLogin = (ProgressBar) findViewById(R.id.progressLogin);
+		    btnLogin = (Button) findViewById(R.id.buttonLogin);
+	        btnLogin.setOnClickListener(new OnClickListener() {
+	                @Override
+	                public void onClick(View arg0) {
+	                        pbLogin.setVisibility(ProgressBar.VISIBLE);
+	                        setConnection();
+	                        getID(txtUserName, pbLogin);
+	                }
+	        });
+	    }
 	}
 }
